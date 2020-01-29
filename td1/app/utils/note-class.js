2fe20cb29a144244e0baa94c861979976513a37c
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-01-29 17:06:09 +0100

    Application de notes

---
+++ td1/app/utils/note-class.js
@@ -0,0 +1,16 @@
+import EmberObject, { computed } from '@ember/object';
+
+export default EmberObject.extend({
+  MAX: 100,
+  content: '',
+  info: '',
+  size: computed("content", function () {
+    return this.get("MAX") - this.get("content").length;
+  }),
+  style: computed("content", function () {
+    return this.get("size") < 50 ? this.get("size") < 20 ? "alert-danger" : "alert-warning" : "alert-info";
+  }),
+  alertVisible: computed("info", function () {
+    return this.get("info").length !== 0;
+  })
+});
