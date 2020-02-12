8c25c118f18fe296da6a495eb4b3026a5695caeb
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-02-12 09:13:19 +0100

    Clean repo

--- td2/app/helpers/array-contains.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function arrayContains(params) {
-  const [items, value] = params;
-  return items.includes(value);
-}
-
-export default Ember.Helper.helper(arrayContains);
