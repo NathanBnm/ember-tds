a33c90139e2c61423913c9d868ac67190f60156b
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-03-27 09:15:57 +0100

    Corrections

--- app/routes/project/board.js
+++ app/routes/project/board.js
@@ -6,8 +6,8 @@ export default Route.extend(
     model() {
       let {project_id} = this.paramsFor('project');
       return RSVP.hash({
-          project: this.get('store').find('project', project_id),
-          steps: this.get('store').findAll('step').then(steps => steps.sortBy('order'))
+        project: this.get('store').find('project', project_id),
+        steps: this.get('store').findAll('step').then(steps => steps.sortBy('order'))
       })
     }
   }
